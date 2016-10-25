52f2b674383456c138a3ddca8924e39d5419e293
DjKirkovic <kirkovic@gmail.com>
2016-10-25 13:35:16 +0200

    DBFacade - get buildings

--- src/java/serviceLayer/enties/Building.java
+++ src/java/serviceLayer/enties/Building.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package serviceLayer.enties;
 
-/**
- *
- * @author Ceo
- */
 public class Building {
     
     private int building_id, postcode, user_id;   
