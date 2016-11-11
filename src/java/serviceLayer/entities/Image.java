f885c4e9bc7ec34f1a22af921ddd6db8194beb27
Bearukun <bearukun@gmail.com>
2016-11-11 12:40:49 +0100

    Updates

--- src/java/serviceLayer/entities/Image.java
+++ src/java/serviceLayer/entities/Image.java
@@ -1,7 +1,50 @@
 package serviceLayer.entities;
 
+import java.sql.Blob;
+
 public class Image {
     
-    
+    private int image_id, issue_id;
+    private String img_name;
+    private Blob img_file;
+
+    public Image(int image_id, int issue_id, String img_name, Blob img_file) {
+        this.image_id = image_id;
+        this.issue_id = issue_id;
+        this.img_name = img_name;
+        this.img_file = img_file;
+    }
+
+    public int getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(int image_id) {
+        this.image_id = image_id;
+    }
+
+    public int getIssue_id() {
+        return issue_id;
+    }
+
+    public void setIssue_id(int issue_id) {
+        this.issue_id = issue_id;
+    }
+
+    public String getImg_name() {
+        return img_name;
+    }
+
+    public void setImg_name(String img_name) {
+        this.img_name = img_name;
+    }
+
+    public Blob getImg_file() {
+        return img_file;
+    }
+
+    public void setImg_file(Blob img_file) {
+        this.img_file = img_file;
+    }
     
 }
